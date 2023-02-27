3969a470634e0a46c9daa243e92fe6ca878eed43
samuhuse <samuele.mn@hotmail.it>
2023-02-27 20:55:44 +0100

    Edited program.cs to build

--- Learning/Console/Program.cs
+++ Learning/Console/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace Console
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿Console.WriteLine("Hello, World!");
