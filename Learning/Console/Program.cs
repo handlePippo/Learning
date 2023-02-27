e22b08e32b28133d4bdbb57aa317449385237032
samuhuse <samuele.mn@hotmail.it>
2023-02-27 22:25:55 +0100

    First lesson: Syntax

--- Learning/Console/Program.cs
+++ Learning/Console/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using static Theory.OOP.Syntax;
+
+
+Person filippo = new Person("Filippo", 25);
+filippo.Phone = "3938904156";
+
+Person samuele = new Person("Samuele", 24, "3938904157", "1234");
+
+Console.WriteLine("Il pin di samuele e' " + samuele.Pin);
+Console.ReadKey();
+
---
+++ Learning/Theory/OOP/Syntax.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace Theory.OOP
+{
+    public class Syntax
+    {
+        public class Person
+        {
+
+            // Costruttori
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public Person(string name, int age, string phone, string pin)
+            {
+                Name = name;
+                Age = age;
+                Phone = phone;
+                Pin = pin;
+            }
+
+            // Variabile
+
+            public string Name = "Nome di default";
+            public int Age;
+            public string Phone;
+
+            // Porpieta'
+            public string Pin { get; } = "1234";
+
+            // Metodo
+            public void Call()
+            {
+                Console.WriteLine("Sto chiamando " + Name + "...");
+            }
+        }
+
+
+    }
+}
