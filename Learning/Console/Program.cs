dc22c952607a63531f720bc5cc1193df2df944a0
Filippo Palliani <filippopallianimail@gmail.com>
2023-02-28 13:44:26 +0100

    esercizio

--- Learning/Console/Program.cs
+++ Learning/Console/Program.cs
@@ -1,11 +1,16 @@
-﻿using static Theory.OOP.Syntax;
+﻿using Excerices;
 
+//Person filippo = new Person("Filippo", 25);
+//filippo.Phone = "3938904156";
 
-Person filippo = new Person("Filippo", 25);
-filippo.Phone = "3938904156";
+//Person samuele = new Person("Samuele", 24, "3938904157", "1234");
 
-Person samuele = new Person("Samuele", 24, "3938904157", "1234");
+//Console.WriteLine("Il pin di samuele e' " + samuele.Pin);
+//Console.ReadKey();
 
-Console.WriteLine("Il pin di samuele e' " + samuele.Pin);
-Console.ReadKey();
+Persona filippo = new Persona();
 
+filippo.FirstNumber = 120;
+filippo.SecondNumber = 120;
+
+filippo.Sum();
---
+++ Learning/Excerices/Persona.cs
@@ -0,0 +1,48 @@
+﻿namespace Excerices
+{
+    public class Persona {
+
+        public int results;
+        public int firstNumber;
+        public int secondNumber;
+
+        public int FirstNumber {
+            get {
+                return firstNumber;
+            }
+            set
+            {
+                firstNumber = value;
+            }
+        }
+
+        public int SecondNumber
+        {
+            get
+            {
+                return secondNumber;
+            }
+            set
+            {
+                secondNumber = value;
+            }
+        }
+
+        public void Sum()
+        {
+            if (this.firstNumber == this.secondNumber)
+            {
+                results = (this.firstNumber + this.secondNumber) * 3;
+            }
+            else
+            {
+                results = this.firstNumber + this.secondNumber;
+            }
+
+            Console.WriteLine("La somma è " + results);
+        }
+
+
+        public Persona() { }
+    }
+}
