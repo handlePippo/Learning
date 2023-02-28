e971d03de48e1b7ef7c3d0f6ccd6ced48a0ccb54
samuhuse <samuele.mn@hotmail.it>
2023-02-28 14:50:58 +0100

    exercises template; Basic es 3

---
+++ Learning/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Excerices;
+
+using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Basics.Es3();
+        }
+    }
+}
