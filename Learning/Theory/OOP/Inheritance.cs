5cf5c1438989f4cf27a6d3334b8609575d4eb828
samuhuse <samuele.mn@hotmail.it>
2023-03-06 22:07:07 +0100

    static, factory method, inheritance

---
+++ Learning/Theory/OOP/Inheritance.cs
@@ -0,0 +1,13 @@
+﻿namespace Theory.OOP
+{
+    public class Inheritance
+    {
+        public class Animal
+        {
+            public DateTime Birth { get; set; }
+            public TimeSpan Age => TimeSpan.FromTicks((DateTime.Now - Birth).Ticks);
+        }
+
+
+    }
+}
--- Learning/Theory/OOP/Syntax.cs
+++ Learning/Theory/OOP/Syntax.cs
@@ -4,14 +4,28 @@ namespace Theory.OOP
 {
     public class Syntax
     {
+        // classe statica, Singleton
+        public static class Util
+        {
+            private static int _count = 0;
+            public static void Greet(string name)
+            {
+                _count++;
+                Console.WriteLine("Ciao " + name + " hai salutato "+ _count + " volte" );
+            }
+        }
+
         public class Person
         {
+            private static int _personCount = 0;
 
             // Costruttori
             public Person(string name, int age)
             {
                 Name = name;
                 Age = age;
+
+                _personCount++;
             }
 
             public Person(string name, int age, string phone, string pin)
@@ -20,11 +34,30 @@ namespace Theory.OOP
                 Age = age;
                 Phone = phone;
                 Pin = pin;
+
+                _personCount++;
+            }
+
+
+            public static Person FromName(string name)
+            {
+                return new Person(name, 0);
+            }
+
+            public static Person FromCF(string cf)
+            {
+                var person =  new Person("", 0);
+
+                person.CF = cf;
+
+                return person;
             }
 
             // Variabile
 
             public string Name = "Nome di default";
+            public string CF;
+
             public int Age;
             public string Phone;
 
@@ -35,9 +68,8 @@ namespace Theory.OOP
             public void Call()
             {
                 Console.WriteLine("Sto chiamando " + Name + "...");
+                Console.WriteLine("esistono " + _personCount + " persone");
             }
         }
-
-
     }
 }
