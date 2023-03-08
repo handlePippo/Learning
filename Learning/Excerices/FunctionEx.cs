5336b5d4007209eacd1c328bd7b0bfaeea960a68
samuhuse <samuele.mn@hotmail.it>
2023-03-08 19:13:01 +0100

    Improved FunctionEX es9, exception handling

--- Learning/Excerices/FunctionEx.cs
+++ Learning/Excerices/FunctionEx.cs
@@ -8,43 +8,48 @@ namespace Excercices
 {
     public static class FunctionEx
     {
+
         public static void Es9()
         {
-            try {
-                List<int> container = new List<int>();
-                Console.WriteLine("Inserisci un numero!");
-                string parseToInt = Console.ReadLine();
-                int parsedNumber = int.Parse(parseToInt);
+            int parsedNumber = default;
 
-                for (int i = 1; i <= parsedNumber; i++)
-                {
-                    if (parsedNumber % i == 0)
-                    {
-                        container.Add(i);
-                    }
-                }
-
-                if (container.Count > 2)
-                {
-                    Console.WriteLine("Il numero inserito non è primo");
-                }
-                else
-                {
-                    Console.WriteLine("Il numero inserito è primo");
-                }
-
-            } catch (Exception ex) {
-
+            List<int> container = new List<int>();
+            Console.WriteLine("Inserisci un numero!");
+            string parseToInt = Console.ReadLine();
+            try
+            {
+                parsedNumber = int.Parse(parseToInt);
+            }
+            catch(FormatException)
+            {
                 Console.Write("Carattere inserito non valido, riprova con un numero!");
                 Es9();
             }
+            catch(Exception ex)
+            {
+                Console.Write($"Oops qualcosa e' andato storto. error: {ex.Message}");
+            }
+
+            for (int i = 1; i <= parsedNumber; i++)
+            {
+                if (parsedNumber % i == 0)
+                {
+                    container.Add(i);
+                }
+            }
+
+            if (container.Count > 2) Console.WriteLine("Il numero inserito non è primo");
+            else Console.WriteLine("Il numero inserito è primo");     
 
         }
 
         //////////////////////////////////////////////////////////////////////////////
 
 
-        public static void Es11() {
+        public static void Es11() 
+        {
+
+            // Gerstire ecccezione sul metodo int.Parse
 
             Console.WriteLine("Inserisci un numero!");
             string number = Console.ReadLine();
